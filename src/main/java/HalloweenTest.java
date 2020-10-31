1d5ff0d3917e988b1aff1cab24a38e25767458bd
Harenateweldebrhan <Haru7095@gmail.com>
2020-10-31 12:39:45 -0400

    2nd commit

--- src/main/java/HalloweenTest.java
+++ src/main/java/HalloweenTest.java
@@ -2,5 +2,10 @@ public class HalloweenTest {
     public static void main(String[] args) {
 
         System.out.println("i am about to give everyone a candy");
+    // hey how are you today
+        // let's play a game
+// it's fun to drive
+        // lets go to church
+
     }
 }
